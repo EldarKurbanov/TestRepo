0349606f5e7a5b22feaf2ba6303ed80ebe970cbe
itschool-vlg <volgograd@myitschool.ru>
2020-10-08 16:08:55 +0400

    New line

--- src/ru/itschool/Main.java
+++ src/ru/itschool/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
         int x = 0;
         while (x < n) {
