9f37b734c47749fed00b24c1a8bcb11d8182a79c
itschool-vlg <volgograd@myitschool.ru>
2020-10-08 16:07:56 +0400

    Initial commit

---
+++ src/ru/itschool/Main.java
@@ -0,0 +1,25 @@
+package ru.itschool;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+        while (x < n) {
+            int height = sc.nextInt();
+            if (height < 437) {
+                System.out.println(x + 1);
+                break;
+            }
+            x++;
+        }
+        if (x == n) {
+            System.out.println("No crash");
+        }
+
+
+    }
+}
